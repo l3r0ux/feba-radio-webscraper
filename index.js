d2eba4a0d1c857a185eae3081653f9ed8f0b0d40
Le Roux Vermeulen <lerouxvermeulen@gmail.com>
2023-04-19 20:06:17 +0200

    udpate port variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const axios = require('axios')
 const cheerio = require('cheerio');
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/get-daily-prayer', async (req, res) => {
     const prayerParam = req.query.currentDate
